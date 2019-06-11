745046978f83b881d9e020c92af3a6cd6458c4c9
August Lounibos <augustbbl@gmail.com>
2019-06-11 15:34:21 -0700

    revert server.js because noone on this project uses branches we are very functional

--- server.js
+++ server.js
@@ -52,10 +52,8 @@ app.get('/login', function (req, res, next) {
   res.status(200).render('account');
 });
 
-var classListMock = { classList: [ {className: "ads"},{className: "ads"},{className: "ads"},{className: "ads"},{className: "ads"},{className: "ads"},{className: "ads"},{className: "ads"},{className: "ads"},{className: "ads"},{className: "ads"},{className: "ads"},{className: "ads"},{className: "ads"}, {className: "asdfds"} ] };
-
 app.get('/check', function (req, res, next) {
-  res.status(200).render('viewPossible', classListMock);
+  res.status(200).render('viewPossible');
 });
 
 // If invalid page
