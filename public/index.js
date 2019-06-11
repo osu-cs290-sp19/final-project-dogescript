fc4013c5210b8baebe4be9393ddf1faa1b794504
JoeyDidner <43554259+JoeyDidner@users.noreply.github.com>
2019-06-10 21:18:52 -0700

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@
 // Function to save all inputs into the forms
 function autoSave() {
   var cTaken = document.getElementById('classesTaken').value.trim();
-  var cWanted = document.getElementById('clasesWanted').value.trim();
+  var cWanted = document.getElementById('classesWanted').value.trim();
 
   var postRequest = new XMLHttpRequest();
   var requestURL = '/addClasses';
@@ -30,3 +30,40 @@ function autoSave() {
   postRequest.setRequestHeader('Content-Type', 'application/json');
   postRequest.send(requestBody);
 }
+
+//Getting Class Data for the viewPossible page
+function getClassData() {
+	var classesTaken = [];
+	var classesWanted = [];
+
+	//var classNames = Handlebars.templates.home;
+
+	var classTakenNames = document.getElementsById("classesTaken");
+	var classWantedNames = document.getElementsById("classesWanted");
+
+	var classContainer = document.getElementsByClassName("Class-container");
+
+	classContainer[0].insertAdjacentHTML('beforeend', classTakenNames);
+	}
+
+//This function goes through the mongodb database and selects a background color for the boxes depending on if the preReq was met with class pages or not
+function changeColor(classTaken, classWanted, preReqs) {
+
+	pR = db.user.find({preReqsNeeded: preReqs});
+	cT = db.user.find({classesTaken: classTaken});
+
+	if(cT == pR) {
+		var cardColor = document.getElementbyId("class-card");
+		cardColor.style.color="green";
+		}
+	else if(pR == {}) {
+		var cardColor = document.getElementbyId("class-card");
+		cardColor.style.color="green";
+		}
+	else{
+		var cardColor = document.getElementbyId("class-card");
+		cardColor.style.color="red";
+
+		}
+	
+	}
